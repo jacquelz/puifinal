4cbae7be81ca6278b5b38fb6d8a47eb479031de1
jacquelz <60626447+jacquelz@users.noreply.github.com>
2020-05-03 01:37:49 -0400

    Add files via upload

---
+++ assets/JS/main.js
@@ -0,0 +1,47 @@
+//philips
+//change gamification slide
+function changeGamification(){
+  document.getElementById("findings").src="assets/images/gamification.png"
+}
+
+//change social slide
+function changeSocial(){
+  document.getElementById("findings").src="assets/images/social.png"
+}
+
+//change personalization slide
+function changePersonalization(){
+  document.getElementById("findings").src="assets/images/personalization.png"
+}
+
+//change notifications slide
+function changeNotification(){
+  document.getElementById("findings").src="assets/images/notifications.png"
+}
+
+
+//upcycled
+//change layer 1
+function changeLayer1(){
+  document.getElementById("layers").src="assets/images/floorplan.jpg"
+}
+
+//change layer 2
+function changeLayer2(){
+  document.getElementById("layers").src="assets/images/tech.jpg"
+}
+
+//change layer 3
+function changeLayer3(){
+  document.getElementById("layers").src="assets/images/techfloor.jpg"
+}
+
+//change layer 4
+function changeLayer4(){
+  document.getElementById("layers").src="assets/images/together.jpg"
+}
+
+//change layer 5
+function changeLayer5(){
+  document.getElementById("layers").src="assets/images/object.jpg"
+}
